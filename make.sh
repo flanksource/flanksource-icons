c46589ee3ceb938929e149e24a4213d8b1a18814
Moshe Immermam <moshe@flanksource.com>
2024-01-23 11:19:46 +0200

    fix: build

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 sed -i 's|"Icon" +||' node_modules/create-react-icons/lib/generate-icons.js
 rm -rf src/
-create-react-icons -s 'svg/*.svg' -d src
+create-react-icons -s 'svg/*.svg' -d src --typescript
