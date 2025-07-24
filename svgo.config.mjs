e725f4fe3500df4bfea90f331f12ef6fef25740a
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-07-24 08:55:41 +0530

    chore: add mcp icon

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -1,129 +1,129 @@
 var prefixId = 0
 export default {
-  multipass: true,
-  eol: 'lf',
-  plugins: [
-    'cleanupAttrs',
-    'inlineStyles',
-    'removeXlink',
-    'removeDoctype',
-    'removeXMLProcInst',
-    'removeComments',
-    'removeMetadata',
-    'removeDesc',
-    'removeUselessDefs',
-    'removeEditorsNSData',
-    'removeEmptyAttrs',
-    'removeHiddenElems',
-    'removeEmptyText',
-    'removeEmptyContainers',
-    'cleanupEnableBackground',
-    {
-      name: 'convertPathData',
-      params: {
-        // 3 decimals of precision in floating point numbers
-        floatPrecision: 3,
-        // Some editors (e.g. Adobe Illustrator and Sketch) cannot parse flags
-        // without space wrapping
-        noSpaceAfterFlags: false,
-      },
-    },
-    'convertTransform',
-    {
-      name: 'removeUnknownsAndDefaults',
-      params: {
-        // Keep the 'role' attribute, if it's already defined
-        keepRoleAttr: true,
-      },
-    },
-    'removeNonInheritableGroupAttrs',
-    {
-      // Remove paths with fill="none"
-      name: 'removeUselessStrokeAndFill',
-      params: {
-        removeNone: true,
-      },
-    },
-    'removeUselessStrokeAndFill',
-    'removeUnusedNS',
-    'cleanupNumericValues',
-    'cleanupListOfValues',
-    'moveGroupAttrsToElems',
-    'collapseGroups',
-    // 'removeRasterImages',
-    {
-      // Compound all <path>s into one
-      name: 'mergePaths',
-      params: {
-        force: true,
-        noSpaceAfterFlags: false,
-      },
-    },
-    {
-      // Convert basic shapes (such as <circle>) to <path>
-      name: 'convertShapeToPath',
-      params: {
-        // including <arc>
-        convertArcs: true,
-      },
-    },
-    'convertEllipseToCircle',
-    {
-      // Sort the attributes on the <svg> tag
-      name: 'sortAttrs',
-      params: {
-        order: ['role', 'viewBox'],
-        xmlnsOrder: 'end',
-      },
-    },
-    'sortDefsChildren',
-    // 'removeDimensions',
-    {
-      name: 'removeAttrs',
-      params: {
-        attrs: [
-          'svg:(?!(role|xmlns))',
-          'path:(?!d)',
-          'title:*',
-          'svg:xmlns.xlink',
-          'path:fill:black',
-          'path:fill:rgba(0,0,0,0)',
-          'path:fill:rgb\\(0%, 0%, 0%\\)',
-          'path:fill:"rgb\\(0%, 0%, 0%\\)"',
-          'path:fill:#000000',
-        ],
-      },
-    },
-    'removeElementsByAttr',
-    {
-      // Keep the role="img" attribute and automatically add it
-      // to the <svg> tag if it's not there already
-      name: 'addAttributesToSVGElement',
-      params: {
-        attributes: [{ role: 'img', xmlns: 'http://www.w3.org/2000/svg' }],
-      },
-    },
-    'removeOffCanvasPaths',
-    'removeStyleElement',
-    'removeScriptElement',
-    {
-      name: "addAttributesToSVGElement",
-      params: {
-        attributes: [{
-          width: "32",
-          height: "32",
-        }]
-      },
-    },
-    {
-      name: 'prefixIds',
-      params: {
-        prefix: function (a, b) {
-          console.log(a, b)
-          return prefixId++
-        }
-      }
-    },
-    // 'reusePaths',
-  ],
+    multipass: true,
+    eol: 'lf',
+    plugins: [
+        'cleanupAttrs',
+        'inlineStyles',
+        'removeXlink',
+        'removeDoctype',
+        'removeXMLProcInst',
+        'removeComments',
+        'removeMetadata',
+        'removeDesc',
+        'removeUselessDefs',
+        'removeEditorsNSData',
+        'removeEmptyAttrs',
+        'removeHiddenElems',
+        'removeEmptyText',
+        'removeEmptyContainers',
+        'cleanupEnableBackground',
+        {
+            name: 'convertPathData',
+            params: {
+                // 3 decimals of precision in floating point numbers
+                floatPrecision: 3,
+                // Some editors (e.g. Adobe Illustrator and Sketch) cannot parse flags
+                // without space wrapping
+                noSpaceAfterFlags: false,
+            },
+        },
+        'convertTransform',
+        {
+            name: 'removeUnknownsAndDefaults',
+            params: {
+                // Keep the 'role' attribute, if it's already defined
+                keepRoleAttr: true,
+            },
+        },
+        'removeNonInheritableGroupAttrs',
+        {
+            // Remove paths with fill="none"
+            name: 'removeUselessStrokeAndFill',
+            params: {
+                removeNone: true,
+            },
+        },
+        'removeUselessStrokeAndFill',
+        'removeUnusedNS',
+        'cleanupNumericValues',
+        'cleanupListOfValues',
+        'moveGroupAttrsToElems',
+        'collapseGroups',
+        // 'removeRasterImages',
+        {
+            // Compound all <path>s into one
+            name: 'mergePaths',
+            params: {
+                force: true,
+                noSpaceAfterFlags: false,
+            },
+        },
+        {
+            // Convert basic shapes (such as <circle>) to <path>
+            name: 'convertShapeToPath',
+            params: {
+                // including <arc>
+                convertArcs: true,
+            },
+        },
+        'convertEllipseToCircle',
+        {
+            // Sort the attributes on the <svg> tag
+            name: 'sortAttrs',
+            params: {
+                order: ['role', 'viewBox'],
+                xmlnsOrder: 'end',
+            },
+        },
+        'sortDefsChildren',
+        // 'removeDimensions',
+        {
+            name: 'removeAttrs',
+            params: {
+                attrs: [
+                    'svg:(?!(role|xmlns))',
+                    'path:(?!d)',
+                    'title:*',
+                    'svg:xmlns.xlink',
+                    'path:fill:black',
+                    'path:fill:rgba(0,0,0,0)',
+                    'path:fill:rgb\\(0%, 0%, 0%\\)',
+                    'path:fill:"rgb\\(0%, 0%, 0%\\)"',
+                    'path:fill:#000000',
+                ],
+            },
+        },
+        'removeElementsByAttr',
+        {
+            // Keep the role="img" attribute and automatically add it
+            // to the <svg> tag if it's not there already
+            name: 'addAttributesToSVGElement',
+            params: {
+                attributes: [{ role: 'img', xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+        },
+        'removeOffCanvasPaths',
+        'removeStyleElement',
+        'removeScripts',
+        {
+            name: "addAttributesToSVGElement",
+            params: {
+                attributes: [{
+                    width: "32",
+                    height: "32",
+                }]
+            },
+        },
+        {
+            name: 'prefixIds',
+            params: {
+                prefix: function(a, b) {
+                    console.log(a, b)
+                    return prefixId++
+                }
+            }
+        },
+        // 'reusePaths',
+    ],
 };
